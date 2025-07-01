e5d4cc484efd2949253aa69da0ec57ac7051f453
yudhanbk <yudhanbishwakarma10@gmail.com>
2025-07-01 16:52:37 +0545

    worked on models

--- Photogallary/gallary/models.py
+++ Photogallary/gallary/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    descriptions = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
